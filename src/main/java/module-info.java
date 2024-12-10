7fcab68225ee8310745e67ed722789c7f3bc2d01
WValleyy <101030063+WValleyy@users.noreply.github.com>
2024-12-10 11:18:58 +0700

    commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.pc_app {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.pc_app to javafx.fxml;
+    exports com.example.pc_app;
+}
